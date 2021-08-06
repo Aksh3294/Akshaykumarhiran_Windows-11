7999b977a3331236cfb0f957677b6f9866a6c3e9
Akshay kumar hiran <83747927+Aksh3294@users.noreply.github.com>
2021-08-06 13:20:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+taskbar= document.getElementsByClassName("taskbar")[0]
+startmenu=document.getElementsByClassName("startmenu")[0]
+
+taskbar.addEventListener("click",()=>{
+    if(startmenu.style.bottom=="50px"){
+        startmenu.style.bottom="-655px"
+    }
+    else{
+        startmenu.style.bottom="50px"
+    }
+})
